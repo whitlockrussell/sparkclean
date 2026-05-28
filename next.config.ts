7fe6eaadf7fc57972f09f51c112508aa66b2063d
Russell Whitlock <whitlock.russell@gmail.com>
2026-05-28 17:26:23 -0400

    add service worker for PWA install

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/],
+})
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+module.exports = withPWA(nextConfig)
