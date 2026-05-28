5a4960c4230b17c62798749272a832ff20e5d0e0
Russell Whitlock <whitlock.russell@gmail.com>
2026-05-28 16:59:39 -0400

    PWA setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default withPWA(nextConfig)
