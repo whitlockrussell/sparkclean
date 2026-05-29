225120e0d3031586f6e57124f9745b3917dff4cd
Russell Whitlock <whitlock.russell@gmail.com>
2026-05-28 22:37:28 -0400

    revert supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,13 +3,6 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookieOptions: {
-        maxAge: 60 * 60 * 24 * 365, // 1 year
-        sameSite: 'lax',
-        secure: true,
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
